db29deaf264cc6ca17b39e442bce0d85dd8f971e
Maruf74 <71781033+Maruf74@users.noreply.github.com>
2022-12-08 13:38:54 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const API_KEY =`19535515-46d1c051007f6c9ef0928da25`;
+const URL = `https://pixabay.com/api/?key=19535515-46d1c051007f6c9ef0928da25&q=yellow+flowers&image_type=photo&pretty=true`;
+let inp = document.querySelector('#inp');
+let btn = document.querySelector('#btn');
+let out = document.querySelector('#output');
+
+
+let id = 0;
+
+const getURL = (query) => {
+    return `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&pretty=true`;
+};
+
+const getPhotoHTML = (ul) => {
+    id++
+    return `<div class="col-2 mt-3">
+            <div class="rounded shadow border">
+            <button type="button" class="btn w-100" data-bs-toggle="modal" data-bs-target="#${id}">
+            <img width="100%" height="150" src="${ul}" alt="">
+            </button>
+                    #${id}
+            </div>
+        </div>
+        
+
+
+        <div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+        
+        
+        
+        
+        
+        
+        `;
+};
+
+
+
+const lodPhoto = (query) => {
+    out.innerHTML =" "
+    axios
+        .get(getURL(query))
+        .then((res) =>{
+            const photos = res.data.hits;
+            console.log(photos[0].id);
+
+            photos.forEach((photo) =>{
+                out.innerHTML += getPhotoHTML(photo.previewURL)
+            })
+
+        })
+}
+
+
+
+btn.addEventListener('click',()=>{
+    let query = inp.value;
+    if(query.length>0){
+        lodPhoto(query)
+        
+    }
+})
