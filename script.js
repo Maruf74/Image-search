64baf80c54cd4cfde5c883d83e3debb3301e84df
Maruf74 <71781033+Maruf74@users.noreply.github.com>
2022-12-08 14:17:59 +0600

    done

--- script.js
+++ script.js
@@ -11,27 +11,28 @@ const getURL = (query) => {
     return `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&pretty=true`;
 };
 
-const getPhotoHTML = (ul) => {
+const getPhotoHTML = (ul, lul) => {
     id++
     return `<div class="col-2 mt-3">
             <div class="rounded shadow border">
-            <button type="button" class="btn w-100" data-bs-toggle="modal" data-bs-target="#${id}">
+            <button type="button" class="btn w-100" data-bs-toggle="modal" data-bs-target="#id${id}">
             <img width="100%" height="150" src="${ul}" alt="">
             </button>
-                    #${id}
+                    ${id}
             </div>
         </div>
         
 
 
-        <div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+        <div  class="modal fade" id="id${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-          <div class="modal-content">
+          <div style="width:900px;" class="modal-content">
             <div class="modal-header">
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              
+            <img width="100%" height="600" src="${lul}" alt="">
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -56,10 +57,10 @@ const lodPhoto = (query) => {
         .get(getURL(query))
         .then((res) =>{
             const photos = res.data.hits;
-            console.log(photos[0].id);
-
+            console.log(photos);
+            
             photos.forEach((photo) =>{
-                out.innerHTML += getPhotoHTML(photo.previewURL)
+                out.innerHTML += getPhotoHTML(photo.previewURL, photo.largeImageURL)
             })
 
         })
